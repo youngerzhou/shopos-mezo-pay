69912f974a8a0b751259f2423c9ed64e22836699
youngerzhou <795886@qq.com>
2026-04-26 23:57:45 +0100

    feat: Update dependencies and Tailwind CSS config
    Updates project dependencies, including Radix UI components, and upgrades Tailwind CSS to v4.0.0. Also adds the `html5-qrcode` library.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -45,7 +45,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        ...props
+        {...props}
       />
     )
   }
